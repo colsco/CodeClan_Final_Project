853f3ddb25b4bfaeef271c60975886010a0129c2
Colin Scotland <colin.s.scotland@gmail.com>
2022-05-27 16:36:14 +0100

    Excalidraw data overview added

--- data_cleaning/cleaning.R
+++ data_cleaning/cleaning.R
@@ -1,3 +1,14 @@
 library(tidyverse)
 library(janitor)
 library(here)
+library(readxl)
+
+
+here::here()
+
+
+
+excel_sheets(here("data/International_Labour_Productivity_Europe.xls"))
+excel_sheets(here("data/UK_Education_Productivity.xlsx"))
+
+
