630ef3ed1cde1e658bac222329f9f86907a0d068
Colin Scotland <colin.s.scotland@gmail.com>
2022-06-02 18:33:49 +0100

    Start of second explanatory model

--- data_cleaning/cleaning.R
+++ data_cleaning/cleaning.R
@@ -447,7 +447,7 @@ model_base_commute <- model_base_data %>%
 
 
 model_commute_region <- model_base_commute %>% 
-  filter(!(region %in% c("Total", "Workplace otuside UK"))) %>% 
+  filter(region != "Workplace otuside UK") %>% 
   left_join(total_commutes, by = "region") %>% 
   group_by(region)
 
@@ -513,6 +513,33 @@ eu_NA <- eu_lab_edu %>%
   summarise(across(.cols = everything(), ~ sum(is.na(.x))))
 
 # No NAs.
+
+# Second Explanatory Model Setup
+
+base_2 <- region_by_industry_output_per_hour %>% 
+  filter(region == "uk" & industry == "ALLINDUSTRIES") %>% 
+  select(-industry)
+
+education_uk <- education %>% 
+  filter(country == "United Kingdom") %>% 
+  filter(year < 2017)
+
+base_2 <- base_2 %>% 
+  bind_cols(education_uk$education_score) %>% 
+  rename("education_score" = "...4")
+
+
+jobs_base_2 <- jobs_regional_bound %>% 
+  separate(quarter, into = c("year", "quarter"), sep = " ") %>% 
+  select(-c(quarter, region, industry, industry_group)) %>% 
+  filter(year %in% (1997:2016)) %>% 
+  group_by(year) %>% 
+  summarise(jobs_000 = sum(avg_jobs_000))
+
+base_2 <- base_2 %>% 
+  bind_cols(jobs_base_2$jobs_000) %>% 
+  rename("jobs_000" = "...5")
+
 
 
 # Data models ready for analysis ----
--- data_cleaning/trial_commute_into_uk_model.R
+++ data_cleaning/trial_commute_into_uk_model.R
@@ -7,5 +7,17 @@ education_uk <- education %>%
   filter(year < 2017)
 
 base_2 <- base_2 %>% 
-  inner_join(education_uk, by = "year") %>% 
-  select(-region)
+  bind_cols(education_uk$education_score) %>% 
+  rename("education_score" = "...4")
+
+
+jobs_base_2 <- jobs_regional_bound %>% 
+  separate(quarter, into = c("year", "quarter"), sep = " ") %>% 
+  select(-c(quarter, region, industry, industry_group)) %>% 
+  filter(year %in% (1997:2016)) %>% 
+  group_by(year) %>% 
+  summarise(jobs_000 = sum(avg_jobs_000))
+
+base_2 <- base_2 %>% 
+  bind_cols(jobs_base_2$jobs_000) %>% 
+  rename("jobs_000" = "...5")
